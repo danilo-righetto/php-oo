61e8c9dd2975ad6bb1e5398a619296dc9887e54d
Danilo <danilonewtrue@gmail.com>
2016-08-07 10:20:50 -0300

    Paradigma funcional

--- index.php
+++ index.php
@@ -3,5 +3,13 @@
 
 // PHP - Iniciando com Orientação a Objetos
 
+$input = array(22,23,24,25,26,27,28);
+$filtro = function(int $age){
+    return ($age >= 18);
+}
+
+$output = array_filter($input, $filtro);
+
+print_r($output);
 
 ?>
