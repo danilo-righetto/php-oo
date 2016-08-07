d4974e2c387f5315f1b051fa02be119681799914
Danilo <danilonewtrue@gmail.com>
2016-08-07 13:08:03 -0300

    Paradigma Orientação a Objetos

--- index.php
+++ index.php
@@ -3,6 +3,8 @@
 
 // PHP - Iniciando com Orientação a Objetos
 
+// Paradigma Funcional
+
 $input = array(22,23,24,25,26,27,28);
 $filtro = function(int $age){
     return ($age >= 18);
@@ -11,5 +13,16 @@ $filtro = function(int $age){
 $output = array_filter($input, $filtro);
 
 print_r($output);
+
+// Usando orientação a Objetos
+
+class People{
+    public function getHello($name){
+        return "Hello {$name}!";
+    }
+}
+
+$p = new People;
+echo $p->getHello("Danilo");
 
 ?>
